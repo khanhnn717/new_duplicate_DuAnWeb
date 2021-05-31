fea124de1417297cf7461f5331837e6398a7ff21
Nguyen Khanh <khanhnn717@gmail.com>
2021-05-31 12:57:34 +0700

    duplicate times: 1

---
+++ asset/js/link-to-services-main.js
@@ -0,0 +1,101 @@
+$(document).ready(function(){
+    $('#service-box--1').on('click', function(){
+        $('.banner-img--1').css('display', 'block');
+        $('.banner-content--1').css('display', 'block');
+        $('.bodycontent-1').css('display', 'block');
+        $('#underline-1').css('display', 'block');
+
+        $('.banner-img--2').css('display', 'none');
+        $('.banner-content--2').css('display', 'none');
+        $('.bodycontent-2').css('display', 'none');
+        $('#underline-2').css('display', 'none');
+
+        $('.banner-img--3').css('display', 'none');
+        $('.banner-content--3').css('display', 'none');
+        $('.bodycontent-3').css('display', 'none'); 
+        $('#underline-3').css('display', 'none');
+    })
+    $('#service-box--2').on('click', function(){
+        $('.banner-img--1').css('display', 'none');
+        $('.banner-content--1').css('display', 'none');
+        $('.bodycontent-1').css('display', 'none');
+        $('#underline-1').css('display', 'none');
+
+        $('.banner-img--2').css('display', 'block');
+        $('.banner-content--2').css('display', 'block');
+        $('.bodycontent-2').css('display', 'block');
+        $('#underline-2').css('display', 'block');
+
+        $('.banner-img--3').css('display', 'none');
+        $('.banner-content--3').css('display', 'none');
+        $('.bodycontent-3').css('display', 'none');
+        $('#underline-3').css('display', 'none');
+    })
+    $('#service-box--3').on('click', function(){
+        $('.banner-img--1').css('display', 'none');
+        $('.banner-content--1').css('display', 'none');
+        $('.bodycontent-1').css('display', 'none');
+        $('#underline-1').css('display', 'none');
+
+        $('.banner-img--2').css('display', 'none');
+        $('.banner-content--2').css('display', 'none');
+        $('.bodycontent-2').css('display', 'none');
+        $('#underline-2').css('display', 'none');
+        
+        $('.banner-img--3').css('display', 'block');
+        $('.banner-content--3').css('display', 'block');
+        $('.bodycontent-3').css('display', 'block');
+        $('#underline-3').css('display', 'block');
+    })
+})
+$(document).ready(function(){
+    $('#link1').on('click', function(){
+        $('.banner-img--1').css('display', 'block');
+        $('.banner-content--1').css('display', 'block');
+        $('.bodycontent-1').css('display', 'block');
+        $('#underline-1').css('display', 'block');
+
+        $('.banner-img--2').css('display', 'none');
+        $('.banner-content--2').css('display', 'none');
+        $('.bodycontent-2').css('display', 'none');
+        $('#underline-2').css('display', 'none');
+
+        $('.banner-img--3').css('display', 'none');
+        $('.banner-content--3').css('display', 'none');
+        $('.bodycontent-3').css('display', 'none'); 
+        $('#underline-3').css('display', 'none'); 
+    })
+    $('#link2').on('click', function(){
+        $('.banner-img--1').css('display', 'none');
+        $('.banner-content--1').css('display', 'none');
+        $('.bodycontent-1').css('display', 'none');
+        $('#underline-1').css('display', 'none');
+
+        $('.banner-img--2').css('display', 'block');
+        $('.banner-content--2').css('display', 'block');
+        $('.bodycontent-2').css('display', 'block');
+        $('#underline-2').css('display', 'block');
+
+        $('.banner-img--3').css('display', 'none');
+        $('.banner-content--3').css('display', 'none');
+        $('.bodycontent-3').css('display', 'none');
+        $('#underline-3').css('display', 'none');
+    })
+    $('#link3').on('click', function(){
+        $('.banner-img--1').css('display', 'none');
+        $('.banner-content--1').css('display', 'none');
+        $('.bodycontent-1').css('display', 'none');
+        $('#underline-1').css('display', 'none');
+
+        $('.banner-img--2').css('display', 'none');
+        $('.banner-content--2').css('display', 'none');
+        $('.bodycontent-2').css('display', 'none');
+        $('#underline-2').css('display', 'none');
+        
+        $('.banner-img--3').css('display', 'block');
+        $('.banner-content--3').css('display', 'block');
+        $('.bodycontent-3').css('display', 'block');
+        $('#underline-3').css('display', 'block');
+    })
+})
+
